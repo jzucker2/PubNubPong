65cec52826ec48b1e55be5d95279d53f3b94b373
Jordan Zucker <jordan.zucker@gmail.com>
2016-06-16 13:24:42 -0700

    Working physics with matchmaking!

--- PubNubPong/PubNubPong/MatchViewController.m
+++ PubNubPong/PubNubPong/MatchViewController.m
@@ -32,6 +32,7 @@
 @property (nonatomic, strong) UIDynamicItemBehavior *opponentPaddleDynamicProperties;
 @property (nonatomic, strong) UIDynamicItemBehavior *ballDynamicProperties;
 @property (nonatomic, strong) UIAttachmentBehavior *myAttacher;
+@property (nonatomic, strong) UIAttachmentBehavior *opponentAttacher;
 @end
 
 @implementation MatchViewController
@@ -77,10 +78,130 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - View Lifecyle
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+//    // Reset with double tap gesture
+//    UITapGestureRecognizer *doubleTapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(reset)];
+//    doubleTapGR.numberOfTapsRequired = 2;
+//    doubleTapGR.numberOfTouchesRequired = 2;
+//    [self.view addGestureRecognizer:doubleTapGR];
+    
+    UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapped:)];
+    [self.view addGestureRecognizer:tapGR];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self _initBehaviors];
+}
+
+#pragma mark - Orientation
+
+- (BOOL)shouldAutorotate {
+    return YES;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation {
+    return UIInterfaceOrientationLandscapeLeft;
+}
+
+#pragma mark - Animations
+
+- (void)_initBehaviors {
+    self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    
+    // Start ball off with a push
+    self.pusher = [[UIPushBehavior alloc] initWithItems:@[self.ballView]
+                                                   mode:UIPushBehaviorModeInstantaneous];
+    self.pusher.pushDirection = CGVectorMake(0.5, 1.0);
+    self.pusher.active = YES; // Because push is instantaneous, it will only happen once
+    self.pusher.magnitude = 0.1;
+    [self.animator addBehavior:self.pusher];
+    
+    // Step 1: Add collisions
+    NSMutableArray *items = [@[self.ballView, self.myPaddle] mutableCopy];
+    if (self.opponentPaddle) {
+        [items addObject:self.opponentPaddle];
+    }
+    self.collider = [[UICollisionBehavior alloc] initWithItems:items.copy];
+    self.collider.collisionDelegate = self;
+    self.collider.collisionMode = UICollisionBehaviorModeEverything;
+    self.collider.translatesReferenceBoundsIntoBoundary = YES;
+    [self.animator addBehavior:self.collider];
+    
+    // Step 2: Remove rotation
+    self.ballDynamicProperties = [[UIDynamicItemBehavior alloc]
+                                  initWithItems:@[self.ballView]];
+    self.ballDynamicProperties.allowsRotation = NO;
+    [self.animator addBehavior:self.ballDynamicProperties];
+    
+    self.myPaddleDynamicProperties = [[UIDynamicItemBehavior alloc]
+                                      initWithItems:@[self.myPaddle]];
+    self.myPaddleDynamicProperties.allowsRotation = NO;
+    [self.animator addBehavior:self.myPaddleDynamicProperties];
+    
+    if (self.opponentPaddle) {
+        self.opponentPaddleDynamicProperties = [[UIDynamicItemBehavior alloc] initWithItems:@[self.opponentPaddle]];
+        self.opponentPaddleDynamicProperties.allowsRotation = NO;
+        self.opponentPaddleDynamicProperties.density = 1000.0f;
+        [self.animator addBehavior:self.opponentPaddleDynamicProperties];
+    }
+    
+    // Step 3: Heavy paddle
+    self.myPaddleDynamicProperties.density = 1000.0f;
+    
+    // Step 4: Better collisions, no friction
+    self.ballDynamicProperties.elasticity = 1.0;
+    self.ballDynamicProperties.friction = 0.0;
+    self.ballDynamicProperties.resistance = 0.0;
+    
+    // Step 5: Move paddle
+    CGPoint anchor = CGPointMake(CGRectGetMidX(self.myPaddle.frame), CGRectGetMidY(self.myPaddle.frame));
+    self.myAttacher = [[UIAttachmentBehavior alloc] initWithItem:self.myPaddle
+                                                attachedToAnchor:anchor];
+    [self.animator addBehavior:self.myAttacher];
+    
+    CGPoint opponentAnchor = CGPointMake(CGRectGetMidX(self.opponentPaddle.frame), CGRectGetMidY(self.opponentPaddle.frame));
+    self.opponentAttacher = [[UIAttachmentBehavior alloc] initWithItem:self.opponentPaddle
+                                                attachedToAnchor:opponentAnchor];
+    [self.animator addBehavior:self.opponentAttacher];
+}
+
+#pragma mark - Actions
+
+- (void)tapped:(UIGestureRecognizer *)gr {
+    CGPoint anchorPoint = [gr locationInView:self.view];
+    //    NSString *anchorPointString = NSStringFromCGPoint(anchorPoint);
+    //    NSLog(@"%@", anchorPointString);
+    //    NSDictionary *anchorPointDictionary = @{
+    //                                            @"x": [NSString stringWithFormat:@"%f", anchorPoint.x],
+    //                                            @"y": [NSString stringWithFormat:@"%f", anchorPoint.y]
+    //                                            };
+    //    NSDictionary *message = @{
+    //                              self.myPaddle.uniqueIdentifier: anchorPointDictionary
+    //                              };
+    //    [self.client publish:message toChannel:@"pubnubpong" withCompletion:^(PNPublishStatus * _Nonnull status) {
+    //        NSLog(@"status: %@", status.debugDescription);
+    //    }];
+    self.myAttacher.anchorPoint = anchorPoint;
+    [self.updater updateLocalPlayerPosition:anchorPoint];
+}
+
 #pragma mark - PNPMatchUpdaterDelegate
 
 - (void)matchUpdater:(PNPMatchUpdater *)matchUpdater receivedUpdate:(PNPMatchUpdate *)matchUpdate {
-    
+    // only update opponent
+    if (![matchUpdate.player isLocalPlayerForClient:self.client]) {
+        self.opponentAttacher.anchorPoint = matchUpdate.position;
+//        [self.view setNeedsLayout];
+    }
 }
 
 /*
